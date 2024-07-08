2005be290972d6945b3ee08b94fb1335b5146744
Mahdi Gholipour <mahdi.83.gp@gmail.com>
2024-07-08 09:01:45 +0330

    Initial-Configuration : config express in app.js file and connect to server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function main() {
+  const app = express();
+  const port = process.env.SERVER_PORT;
+  require("./src/configs/mongo.config");
+  app.listen(port, () => {
+    console.log(`server run on port ${port} : https://localhost:${port}`);
+  });
+}
+
+main();
