7803d96b3b2f9302a89bb41979e0657a2a19945d
Mahdi Gholipour <mahdi.83.gp@gmail.com>
2024-07-09 09:25:48 +0330

    User-Model : created user model

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ dotenv.config();
 async function main() {
   const app = express();
   const port = process.env.SERVER_PORT;
+
   require("./src/configs/mongo.config");
+
+  
   app.listen(port, () => {
     console.log(`server run on port ${port} : https://localhost:${port}`);
   });
---
+++ src/modules/user/model.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const OTPSchema = new Schema({
+  code: { type: String, required: false, default: undefined },
+  expiresIn: { type: number, required: false, default: 0 },
+});
+
+const userSchema = new Schema(
+  {
+    fullName: { type: String, required: false },
+    mobile: { type: String, required: true, unique: true },
+    verifiedMobile: { type: Boolean, required: true, default: false },
+    OTP: { type: OTPSchema },
+  },
+  { timestamps: true }
+);
+
+const UserModel = model("user", userSchema);
+
+module.exports = UserModel;
