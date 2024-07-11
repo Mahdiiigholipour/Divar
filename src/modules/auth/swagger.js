09b6fb9d07587cb97134cb380c19a439137b801f
Mahdi Gholipour <mahdi.83.gp@gmail.com>
2024-07-11 21:27:34 +0330

    auth-module : created auth swagger file and developed send otp endpoint

---
+++ src/modules/auth/swagger.js
@@ -0,0 +1,34 @@
+/**
+ * @swagger
+ * tags :
+ *  name : Auth
+ *  description : auth modules and routes
+ */
+
+/**
+ * @swagger
+ *  components : 
+ *      schemas : 
+ *          SendOTP : 
+ *              type : object
+ *              required : 
+ *                  -   mobile
+ *              properties : 
+ *                  mobile : 
+ *                      type : string
+ */
+
+/**
+ * @swagger
+ * /auth/send-otp :
+ *  post :      
+ *      summary : login with OTP 
+ *      tags : 
+ *          -   Auth
+ *      requestBody : 
+ *          content : 
+ *              application/x-www-urlencoded
+ *                  schema : 
+ *                      $ref : "#components/schemas/SendOTP"
+ *                      
+ */
