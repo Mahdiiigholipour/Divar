2a524897efebcaca7dec37ca29632930355593bd
Mahdi Gholipour <mahdi.83.gp@gmail.com>
2024-07-11 21:10:18 +0330

    auth-module : add application form configs to express

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ async function main() {
   const port = process.env.SERVER_PORT;
 
   require("./src/configs/mongo.config");
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   SwaggerConfig(app);
   app.use(mainRouter);
-  
-  
+
   app.listen(port, () => {
     console.log(`server : http://127.0.0.1:${port}`);
   });
