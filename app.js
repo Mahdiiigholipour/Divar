6cc7a3ae67fa33e6280a3bfac74fa1b602f3e845
Mahdi Gholipour <mahdi.83.gp@gmail.com>
2024-07-11 23:54:30 +0330

    EXCEPTION : create exception folder and files, develop and use them in app.js file

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const SwaggerConfig = require("./src/configs/swagger.config");
 const mainRouter = require("./src/app.routes");
+const notFoundRoute = require("./src/common/exceptions/route-exceptions");
+const allExceptionsHandler = require("./src/common/exceptions/all-exceptions");
 dotenv.config();
 
 async function main() {
@@ -14,6 +16,8 @@ async function main() {
   app.use(express.urlencoded({ extended: true }));
   SwaggerConfig(app);
   app.use(mainRouter);
+  allExceptionsHandler(app);
+  notFoundRoute(app);
 
   app.listen(port, () => {
     console.log(`server : http://127.0.0.1:${port}`);
