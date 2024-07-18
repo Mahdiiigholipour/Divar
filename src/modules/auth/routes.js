0144137fc0f11341366325658f293a8acec14e31
Mahdi Gholipour <mahdi.83.gp@gmail.com>
2024-07-18 19:51:39 +0330

    AUTH-MODULE : logout method was developed

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const AuthController = require("./controller");
+const verifyAccessToken = require("../../common/guard/authorization.guard");
 
 router.post("/send-otp", AuthController.sendOTP);
 router.post("/check-otp", AuthController.checkOTP);
+router.get("/logout", verifyAccessToken, AuthController.logout);
 
 module.exports = {
   AuthRoutes: router,
